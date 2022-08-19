a2b8a92c5f5823f82bd9691e58002238e7f7bde5
fabiohild <fabiohildebrand@gmail.com>
2022-08-18 22:43:05 -0300

    proposals 8 and 9

---
+++ src/test/proposals/8.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.6.7;
+pragma experimental ABIEncoderV2;
+
+import "../SimulateProposalBase.t.sol";
+
+abstract contract StreamVaultLike {
+    function streamId() external virtual returns (uint256);
+}
+
+contract Proposal8Test is SimulateProposalBase {
+    function test_proposal_8() public onlyFork {
+
+        StreamVaultLike streamVault = StreamVaultLike(0x0FA9c7Ad448e1a135228cA98672A0250A2636a47); // GEB_DAO_STREAM_VAULT
+
+        assertTrue(streamVault.streamId() != 0); // active
+
+        // packing data for the proposal
+        address[] memory targets = new address[](1);
+        bytes[] memory calldatas = new bytes[](1);
+
+        targets[0] = govActions;
+        calldatas[0] = abi.encodeWithSelector(
+            bytes4(keccak256("cancelStream(address)")),
+            address(streamVault)
+        );
+        _passProposal(targets, calldatas);
+
+        assertEq(streamVault.streamId(), 0); // cancelled
+
+        _logData(targets, calldatas);
+    }
+}

edc6ee1520d0bcf721e93ddf5ef314f63289d4fe
Luke Hamilton <lukehamiltonmail@gmail.com>
2023-01-16 14:42:04 -0500

    proposal 21

---
+++ src/test/proposals/21.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.6.7;
+pragma experimental ABIEncoderV2;
+
+import "../SimulateProposalBase.t.sol";
+
+contract Proposal21Test is SimulateProposalBase {
+    function test_proposal_21() public onlyFork {
+        uint256 currentVotingDelay = 1;
+        uint256 newVotingDelay = 5760; // 24 hours in 15 second blocks
+
+        assertEq(governor.votingDelay(), currentVotingDelay);
+
+        address[] memory targets = new address[](1);
+        bytes[] memory calldatas = new bytes[](1);
+
+        targets[0] = address(governor);
+         calldatas[0] = abi.encodeWithSelector(
+            bytes4(keccak256("_setVotingDelay(uint256)")),
+            newVotingDelay
+        );
+
+        _passProposal(targets, calldatas);
+
+        assertEq(governor.votingDelay(), newVotingDelay);
+    }
+
+}

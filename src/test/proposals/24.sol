5d2eb8a8d193cc0c6f67b6fb20a872746ebd7bbf
Luke Hamilton <lukehamiltonmail@gmail.com>
2023-02-13 22:32:19 -0500

    wip

--- src/test/proposals/24.sol
+++ src/test/proposals/24.sol
@@ -3,16 +3,15 @@ pragma experimental ABIEncoderV2;
 
 import "../SimulateProposalBase.t.sol";
 
-contract Proposal20Test is SimulateProposalBase {
-    function test_proposal_20() public onlyFork {
+contract Proposal24Test is SimulateProposalBase {
+    function test_proposal_24() public onlyFork {
 
         address[] memory targets = new address[](1);
         bytes[] memory calldatas = new bytes[](1);
 
         address oracleOverlay = 0xBf26309B0BA639ABE651dd1e1042Eb3C57c3e100;
 
-        // TODO: Replace with address of newly deployed Tellor Oracle
-        address updatedTellorOracle = 0x1234567890123456789012345678901234567890;
+        address updatedTellorOracle = 0x58881e5bbecA2F1186921Ae86149edaCc717429A;
 
         targets[0] = oracleOverlay;
         calldatas[0] = abi.encodeWithSignature(
@@ -21,6 +20,8 @@ contract Proposal20Test is SimulateProposalBase {
             uint256(2), // 0 = Chainlink, 1 = Existing Tellor, 2 = Updated Tellor
             address(updatedTellorOracle)
         );
+
+        _logData(targets, calldatas);
        
         // propose / execute proposal
         _passProposal(targets, calldatas);

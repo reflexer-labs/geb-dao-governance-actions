a3ee4f0674b285d26207eea39b4187136c89805a
Luke Hamilton <lukehamiltonmail@gmail.com>
2023-02-20 15:31:15 -0500

    Add oracle methods to gov actions

--- src/GebDaoGovernanceActions.sol
+++ src/GebDaoGovernanceActions.sol
@@ -1,62 +1,173 @@
 pragma solidity ^0.6.7;
 
-abstract contract Setter {
+contract CustomDataTypes {
+    enum ChangeType {
+        Add,
+        Remove,
+        Replace
+    }
+
+    struct Change {
+        ChangeType action;
+        uint256 executionTimestamp;
+        uint256 oracleIndex;
+        address newOracle;
+    }
+}
+
+abstract contract Setter is CustomDataTypes {
     function modifyParameters(bytes32, uint256) external virtual;
+
     function modifyParameters(bytes32, int256) external virtual;
+
     function modifyParameters(bytes32, address) external virtual;
+
     function modifyParameters(bytes32, bytes32, address) external virtual;
+
     function modifyParameters(address, bytes32, uint) external virtual;
+
     function modifyParameters(address, bytes32, address) external virtual;
-    function modifyParameters(address, bytes4, bytes32, uint256) external virtual;
+
+    function modifyParameters(
+        address,
+        bytes4,
+        bytes32,
+        uint256
+    ) external virtual;
+
     function connectSAFESaviour(address) external virtual;
+
     function disconnectSAFESaviour(address) external virtual;
+
     function transferERC20(address, address, uint256) external virtual;
+
     function restartRedemptionRate() external virtual;
+
     function changePriceSource(address) external virtual;
+
     function updateResult(uint256) external virtual;
-    function addFundingReceiver(address, bytes4, uint256, uint256, uint256) external virtual;
-    function addFundingReceiver(address, bytes4, uint256, uint256, uint256, uint256) external virtual;
+
+    function addFundingReceiver(
+        address,
+        bytes4,
+        uint256,
+        uint256,
+        uint256
+    ) external virtual;
+
+    function addFundingReceiver(
+        address,
+        bytes4,
+        uint256,
+        uint256,
+        uint256,
+        uint256
+    ) external virtual;
+
     function removeFundingReceiver(address, bytes4) external virtual;
+
     function addRewardAdjuster(address) external virtual;
+
     function removeRewardAdjuster(address) external virtual;
+
     function addFundedFunction(address, bytes4, uint256) external virtual;
+
     function removeFundedFunction(address, bytes4) external virtual;
-    function createStream(address, uint256, address, uint256, uint256) external virtual;
+
+    function createStream(
+        address,
+        uint256,
+        address,
+        uint256,
+        uint256
+    ) external virtual;
+
     function cancelStream() external virtual;
+
     function mint() external virtual;
+
     function _setVotingDelay(uint) external virtual;
+
+    function swapOracle(uint256) external virtual;
+
+    function ScheduleChangeTrustedOracle(
+        ChangeType,
+        uint256,
+        address
+    ) external virtual;
+
+    function executeChange() external virtual;
+
+    function cancelChange() external virtual;
 }
 
 // @notice Contract to be used by GEB DAO allowing changes in all RAI parameters that were not ungoerned
 // @dev Supposed to be delegatecalled into by the RAI Ungovernor
-contract GebDaoGovernanceActions {
-
-    function modifyParameters(address target, bytes32 param, uint256 val) external {
+contract GebDaoGovernanceActions is CustomDataTypes {
+    function modifyParameters(
+        address target,
+        bytes32 param,
+        uint256 val
+    ) external {
         Setter(target).modifyParameters(param, val);
     }
 
-    function modifyParameters(address target, bytes32 param, int256 val) external {
+    function modifyParameters(
+        address target,
+        bytes32 param,
+        int256 val
+    ) external {
         Setter(target).modifyParameters(param, val);
     }
 
-    function modifyParameters(address target, bytes32 param, address val) external {
+    function modifyParameters(
+        address target,
+        bytes32 param,
+        address val
+    ) external {
         Setter(target).modifyParameters(param, val);
     }
 
-    function modifyParameters(address target, bytes32 collateralType, bytes32 parameter, address data) external {
+    function modifyParameters(
+        address target,
+        bytes32 collateralType,
+        bytes32 parameter,
+        address data
+    ) external {
         Setter(target).modifyParameters(collateralType, parameter, data);
     }
 
-    function modifyParameters(address target, address reimburser, bytes32 parameter, uint256 data) external {
+    function modifyParameters(
+        address target,
+        address reimburser,
+        bytes32 parameter,
+        uint256 data
+    ) external {
         Setter(target).modifyParameters(reimburser, parameter, data);
     }
 
-    function modifyParameters(address target, address reimburser, bytes32 parameter, address data) external {
+    function modifyParameters(
+        address target,
+        address reimburser,
+        bytes32 parameter,
+        address data
+    ) external {
         Setter(target).modifyParameters(reimburser, parameter, data);
     }
 
-    function modifyParameters(address target, address fundingTarget, bytes4 fundedFunction, bytes32 parameter, uint256 data) external {
-        Setter(target).modifyParameters(fundingTarget, fundedFunction, parameter, data);
+    function modifyParameters(
+        address target,
+        address fundingTarget,
+        bytes4 fundedFunction,
+        bytes32 parameter,
+        uint256 data
+    ) external {
+        Setter(target).modifyParameters(
+            fundingTarget,
+            fundedFunction,
+            parameter,
+            data
+        );
     }
 
     function connectSAFESaviour(address target, address saviour) external {
@@ -67,7 +178,12 @@ contract GebDaoGovernanceActions {
         Setter(target).disconnectSAFESaviour(saviour);
     }
 
-    function transferERC20(address target, address token, address dst, uint256 amount) external {
+    function transferERC20(
+        address target,
+        address token,
+        address dst,
+        uint256 amount
+    ) external {
         Setter(target).transferERC20(token, dst, amount);
     }
 
@@ -86,7 +202,7 @@ contract GebDaoGovernanceActions {
     function addFundingReceiver(
         address target,
         address receiver,
-        bytes4  targetFunctionSignature,
+        bytes4 targetFunctionSignature,
         uint256 updateDelay,
         uint256 gasAmountForExecution,
         uint256 fixedRewardMultiplier
@@ -103,7 +219,7 @@ contract GebDaoGovernanceActions {
     function addFundingReceiver(
         address target,
         address receiver,
-        bytes4  targetFunctionSignature,
+        bytes4 targetFunctionSignature,
         uint256 updateDelay,
         uint256 gasAmountForExecution,
         uint256 baseRewardMultiplier,
@@ -119,8 +235,12 @@ contract GebDaoGovernanceActions {
         );
     }
 
-    function removeFundingReceiver(address target, address receiver, bytes4  targetFunctionSignature) external {
-        Setter(target).removeFundingReceiver(receiver,targetFunctionSignature);
+    function removeFundingReceiver(
+        address target,
+        address receiver,
+        bytes4 targetFunctionSignature
+    ) external {
+        Setter(target).removeFundingReceiver(receiver, targetFunctionSignature);
     }
 
     function addRewardAdjuster(address target, address adjuster) external {
@@ -131,11 +251,24 @@ contract GebDaoGovernanceActions {
         Setter(target).removeRewardAdjuster(adjuster);
     }
 
-    function addFundedFunction(address target, address targetContract, bytes4 targetFunction, uint256 latestExpectedCalls) external {
-        Setter(target).addFundedFunction(targetContract, targetFunction, latestExpectedCalls);
+    function addFundedFunction(
+        address target,
+        address targetContract,
+        bytes4 targetFunction,
+        uint256 latestExpectedCalls
+    ) external {
+        Setter(target).addFundedFunction(
+            targetContract,
+            targetFunction,
+            latestExpectedCalls
+        );
     }
 
-    function removeFundedFunction(address target, address targetContract, bytes4 targetFunction) external {
+    function removeFundedFunction(
+        address target,
+        address targetContract,
+        bytes4 targetFunction
+    ) external {
         Setter(target).removeFundedFunction(targetContract, targetFunction);
     }
 
@@ -147,7 +280,13 @@ contract GebDaoGovernanceActions {
         uint256 startTime,
         uint256 stopTime
     ) external {
-        Setter(target).createStream(recipient, deposit, tokenAddress, startTime, stopTime);
+        Setter(target).createStream(
+            recipient,
+            deposit,
+            tokenAddress,
+            startTime,
+            stopTime
+        );
     }
 
     function cancelStream(address target) external {
@@ -161,4 +300,34 @@ contract GebDaoGovernanceActions {
     function _setVotingDelay(address target, uint newVotingDelay) external {
         Setter(target)._setVotingDelay(newVotingDelay);
     }
+
+    function swapOracle(address target, uint256 oracleIndex) external {
+        Setter(target).swapOracle(oracleIndex);
+    }
+
+    function ScheduleChangeTrustedOracle(
+        address target,
+        ChangeType changeType,
+        uint256 oracleIndex,
+        address newOracle
+    ) external {
+        Setter(target).ScheduleChangeTrustedOracle(
+            changeType,
+            oracleIndex,
+            newOracle
+        );
+    }
+
+    function executeChange(address target) external {
+        Setter(target).executeChange();
+    }
+
+    function cancelChange(address target) external {
+        Setter(target).cancelChange();
+    }
+
+    //     swapOracle
+    // ScheduleChangeTrustedOracle
+    // executeChange
+    // cancelChange
 }

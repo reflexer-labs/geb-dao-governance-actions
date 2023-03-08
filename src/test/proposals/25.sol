5ce851475a729a1677218bab492d0f1f98414773
Luke Hamilton <lukehamiltonmail@gmail.com>
2023-03-08 00:09:19 -0500

    wip

--- src/test/proposals/25.sol
+++ src/test/proposals/25.sol
@@ -9,9 +9,14 @@ contract Proposal25Test is SimulateProposalBase {
         address[] memory targets = new address[](1);
         bytes[] memory calldatas = new bytes[](1);
 
+        address oracleOverlay = 0xBf26309B0BA639ABE651dd1e1042Eb3C57c3e100;
+
+        address updatedTellorOracle = 0x58881e5bbecA2F1186921Ae86149edaCc717429A;
+
         targets[0] = govActions;
         calldatas[0] = abi.encodeWithSignature(
             "ScheduleChangeTrustedOracle(address,ChangeType,uint256,address)",
+            oracleOverlay,
             uint8(0), // "ChangeType: Add"
             uint256(2), // 0 = Chainlink, 1 = Existing Tellor, 2 = Updated Tellor
             address(updatedTellorOracle)
